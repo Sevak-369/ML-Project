baddb7a11b4c156f0d7b11ecf0c29d9a94ce1df6
Sevak-369 <devre.r@northeastern.edu>
2025-02-16 18:44:50 -0500

    EDA all questions with histogram visualization

--- Notebook/EDA.py
+++ Notebook/EDA.py
@@ -7,7 +7,7 @@ import warnings
 wasrnings.filterwarnings('ignore')
 
 ### Import Dataset
-df = pd.read_csv("D:/ML Project/Notebook/PM.csv")
+df = pd.read_csv("D:/ML Project/Notebook/stud.csv")
 
 ###EDA
 df.head()
@@ -29,26 +29,20 @@ df.nunique()
 df.describe()
 
 #Exploring the data
-#print("Categories in 'Product ID' varible:     ",end=" ")
-#print(df['Product ID'].unique())
+print("Categories in 'gender' variable:     ",end=" " )
+print(df['gender'].unique())
 
-print("Categories in 'Type' varible:     ",end=" ")
-print(df['Type'].unique())
+print("Categories in 'race_ethnicity' variable:  ",end=" ")
+print(df['race_ethnicity'].unique())
 
-print("Categories in 'Air temperature [K]' varible:     ",end=" ")
-print(df['Air temperature [K]'].unique())
+print("Categories in'parental level of education' variable:",end=" " )
+print(df['parental_level_of_education'].unique())
 
-print("Categories in 'Process temperature [K]' varible:     ",end=" ")
-print(df['Process temperature [K]'].unique())
+print("Categories in 'lunch' variable:     ",end=" " )
+print(df['lunch'].unique())
 
-print("Categories in 'Rotational speed [rpm]' varible:     ",end=" ")
-print(df['Rotational speed [rpm]'].unique())
-
-print("Categories in 'Torque [Nm]' varible:     ",end=" ")
-print(df['Rotational speed [rpm]'].unique())
-
-print("Categories in 'Tool wear [min]' varible:     ",end=" ")
-print(df['Tool wear [min]'].unique())
+print("Categories in 'test preparation course' variable:     ",end=" " )
+print(df['test_preparation_course'].unique())
 
 ##Feature Engineering
 #Define Numerical and Caegorical columns
@@ -57,3 +51,285 @@ categorical_features = [feature for feature in df.columns if df[feature].dtype =
 
 print('we have {} numerical features : {}'.format(len(numeric_features), numeric_features))
 print('\nWe have {} categorical features : {}'.format(len(categorical_features), categorical_features))
+
+df.head(2)
+
+##Adding columns for "Total Score" and "Average"
+df['total score'] = df['math_score'] + df['reading_score'] + df['writing_score']
+df['average'] = df['total score']/3
+df.head()
+
+reading_full = df[df['reading_score'] == 100]['average'].count()
+writing_full = df[df['writing_score'] == 100]['average'].count()
+math_full = df[df['math_score'] == 100]['average'].count()
+
+print(f'Number of students with full marks in maths: {math_full}')
+print(f'Number of students with full marks in writing: {writing_full}')
+print(f'Number of students with full marks in reading_full: {reading_full}')
+
+reading_less_20 = df[df['reading_score'] <= 20]['average'].count()
+writing_less_20 = df[df['writing_score'] <= 20]['average'].count()
+math_less_20 = df[df['math_score'] <= 20]['average'].count()
+
+print(f'Number of students with less than 20 marks in Reading: {math_less_20}')
+print(f'Number of students with less than 20 marks in Writing: {writing_less_20}')
+print(f'Number of students with less than 20 marks in Math: {math_less_20}')
+
+fig, axs = plt.subplots(1, 2, figsize=(15,7))
+
+sns.histplot(data=df, x='average', bins=30, kde=True, color='beige', ax=axs[0])  # First subplot
+sns.histplot(data=df, x='average', kde=True, hue='gender', ax=axs[1])  # Second subplot
+
+plt.show()
+
+
+fig, axs = plt.subplots(1,2, figsize=(15,7))
+
+sns.histplot(data=df, x='total score', bins=30, kde=True,color='blue', ax=axs[0])
+sns.histplot(data=df,x='total score', kde=True, hue='gender', ax=axs[1])
+
+plt.show()
+
+# Create a figure with 3 subplots
+fig, axs = plt.subplots(1, 3, figsize=(25, 6))
+
+# Plot the first histogram (overall distribution)
+sns.histplot(data=df, x='average', kde=True, hue='lunch', ax=axs[0])
+axs[0].set_title("Overall Distribution")
+
+# Plot the second histogram (female distribution)
+sns.histplot(data=df[df.gender == 'female'], x='average', kde=True, hue='lunch', ax=axs[1])
+axs[1].set_title("Female Distribution")
+
+# Plot the third histogram (male distribution)
+sns.histplot(data=df[df.gender == 'male'], x='average', kde=True, hue='lunch', ax=axs[2])
+axs[2].set_title("Male Distribution")
+
+# Display the plots
+plt.show()
+
+plt.subplots(1,3,figsize=(25,6))
+plt.subplot(141)
+ax =sns.histplot(data=df,x='average',kde=True,hue='parental level of education')
+plt.subplot(142)
+ax =sns.histplot(data=df[df.gender=='male'],x='average',kde=True,hue='parental level of education')
+plt.subplot(143)
+ax =sns.histplot(data=df[df.gender=='female'],x='average',kde=True,hue='parental level of education')
+plt.show()
+
+plt.subplots(1,3,figsize=(25,6))
+plt.subplot(141)
+ax =sns.histplot(data=df,x='average',kde=True,hue='race/ethnicity')
+plt.subplot(142)
+ax =sns.histplot(data=df[df.gender=='female'],x='average',kde=True,hue='race/ethnicity')
+plt.subplot(143)
+ax =sns.histplot(data=df[df.gender=='male'],x='average',kde=True,hue='race/ethnicity')
+plt.show()
+
+##Maximumum score of students in all three subjects
+plt.figure(figsize=(18,8))
+plt.subplot(1, 4, 1)
+plt.title('MATH SCORES')
+sns.violinplot(y='math score',data=df,color='red',linewidth=3)
+plt.subplot(1, 4, 2)
+plt.title('READING SCORES')
+sns.violinplot(y='reading score',data=df,color='green',linewidth=3)
+plt.subplot(1, 4, 3)
+plt.title('WRITING SCORES')
+sns.violinplot(y='writing score',data=df,color='blue',linewidth=3)
+plt.show()
+
+
+##Multivariate analysis using pieplot
+plt.rcParams['figure.figsize'] = (30, 12)
+
+plt.subplot(1, 5, 1)
+size = df['gender'].value_counts()
+labels = 'Female', 'Male'
+color = ['red','green']
+
+
+plt.pie(size, colors = color, labels = labels,autopct = '.%2f%%')
+plt.title('Gender', fontsize = 20)
+plt.axis('off')
+
+
+
+plt.subplot(1, 5, 2)
+size = df['race/ethnicity'].value_counts()
+labels = 'Group C', 'Group D','Group B','Group E','Group A'
+color = ['red', 'green', 'blue', 'cyan','orange']
+
+plt.pie(size, colors = color,labels = labels,autopct = '.%2f%%')
+plt.title('Race/Ethnicity', fontsize = 20)
+plt.axis('off')
+
+
+
+plt.subplot(1, 5, 3)
+size = df['lunch'].value_counts()
+labels = 'Standard', 'Free'
+color = ['red','green']
+
+plt.pie(size, colors = color,labels = labels,autopct = '.%2f%%')
+plt.title('Lunch', fontsize = 20)
+plt.axis('off')
+
+
+plt.subplot(1, 5, 4)
+size = df['test preparation course'].value_counts()
+labels = 'None', 'Completed'
+color = ['red','green']
+
+plt.pie(size, colors = color,labels = labels,autopct = '.%2f%%')
+plt.title('Test Course', fontsize = 20)
+plt.axis('off')
+
+
+plt.subplot(1, 5, 5)
+size = df['parental level of education'].value_counts()
+labels = 'Some College', "Associate's Degree",'High School','Some High School',"Bachelor's Degree","Master's Degree"
+color = ['red', 'green', 'blue', 'cyan','orange','grey']
+
+plt.pie(size, colors = color,labels = labels,autopct = '.%2f%%')
+plt.title('Parental Education', fontsize = 20)
+plt.axis('off')
+
+
+plt.tight_layout()
+plt.grid()
+
+plt.show()
+
+###Feature Wise Visualization
+##GENDER COLUMN
+#UNIVARIATE ANALYSIS ( How is distribution of Gender ? )
+f,ax=plt.subplots(1,2,figsize=(20,10))
+sns.countplot(x=df['gender'],data=df,palette ='bright',ax=ax[0],saturation=0.95)
+for container in ax[0].containers:
+    ax[0].bar_label(container,color='black',size=20)
+    
+plt.pie(x=df['gender'].value_counts(),labels=['Male','Female'],explode=[0,0.1],autopct='%1.1f%%',shadow=True,colors=['#ff4d4d','#ff8000'])
+plt.show()
+
+#BIVARIATE ANALYSIS ( Is gender has any impact on student's performance ? )
+gender_group = df.groupby('gender').mean()
+gender_group
+plt.figure(figsize=(10, 8))
+
+X = ['Total Average','Math Average']
+
+
+female_scores = [gender_group['average'][0], gender_group['math score'][0]]
+male_scores = [gender_group['average'][1], gender_group['math score'][1]]
+
+X_axis = np.arange(len(X))
+  
+plt.bar(X_axis - 0.2, male_scores, 0.4, label = 'Male')
+plt.bar(X_axis + 0.2, female_scores, 0.4, label = 'Female')
+  
+plt.xticks(X_axis, X)
+plt.ylabel("Marks")
+plt.title("Total average v/s Math average marks of both the genders", fontweight='bold')
+plt.legend()
+plt.show()
+
+##RACE/EHNICITY COLUMN
+#UNIVARIATE ANALYSIS ( How is Group wise distribution ?)
+f,ax=plt.subplots(1,2,figsize=(20,10))
+sns.countplot(x=df['race/ethnicity'],data=df,palette = 'bright',ax=ax[0],saturation=0.95)
+for container in ax[0].containers:
+    ax[0].bar_label(container,color='black',size=20)
+    
+plt.pie(x = df['race/ethnicity'].value_counts(),labels=df['race/ethnicity'].value_counts().index,explode=[0.1,0,0,0,0],autopct='%1.1f%%',shadow=True)
+plt.show()
+
+#BIVARIATE ANALYSIS ( Is Race/Ehnicity has any impact on student's performance ? )
+Group_data2=df.groupby('race/ethnicity')
+f,ax=plt.subplots(1,3,figsize=(20,8))
+sns.barplot(x=Group_data2['math score'].mean().index,y=Group_data2['math score'].mean().values,palette = 'mako',ax=ax[0])
+ax[0].set_title('Math score',color='#005ce6',size=20)
+
+for container in ax[0].containers:
+    ax[0].bar_label(container,color='black',size=15)
+
+sns.barplot(x=Group_data2['reading score'].mean().index,y=Group_data2['reading score'].mean().values,palette = 'flare',ax=ax[1])
+ax[1].set_title('Reading score',color='#005ce6',size=20)
+
+for container in ax[1].containers:
+    ax[1].bar_label(container,color='black',size=15)
+
+sns.barplot(x=Group_data2['writing score'].mean().index,y=Group_data2['writing score'].mean().values,palette = 'coolwarm',ax=ax[2])
+ax[2].set_title('Writing score',color='#005ce6',size=20)
+
+for container in ax[2].containers:
+    ax[2].bar_label(container,color='black',size=15)
+
+##PARENTAL LEVEL OF EDUCATION COLUMN
+#UNIVARIATE ANALYSIS ( What is educational background of student's parent ? )
+plt.rcParams['figure.figsize'] = (15, 9)
+plt.style.use('fivethirtyeight')
+sns.countplot(df['parental level of education'], palette = 'Blues')
+plt.title('Comparison of Parental Education', fontweight = 30, fontsize = 20)
+plt.xlabel('Degree')
+plt.ylabel('count')
+plt.show()
+
+#BIVARIATE ANALYSIS ( Is parental education has any impact on student's performance ? )
+df.groupby('parental level of education').agg('mean').plot(kind='barh',figsize=(10,10))
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+plt.show()
+
+##LUNCH COLUMN
+#UNIVARIATE ANALYSIS ( Which type of lunch is most common amoung students ? )
+plt.rcParams['figure.figsize'] = (15, 9)
+plt.style.use('seaborn-talk')
+sns.countplot(df['lunch'], palette = 'PuBu')
+plt.title('Comparison of different types of lunch', fontweight = 30, fontsize = 20)
+plt.xlabel('types of lunch')
+plt.ylabel('count')
+plt.show()
+
+#BIVARIATE ANALYSIS ( Is lunch type intake has any impact on student's performance ? )
+f,ax=plt.subplots(1,2,figsize=(20,8))
+sns.countplot(x=df['parental level of education'],data=df,palette = 'bright',hue='test preparation course',saturation=0.95,ax=ax[0])
+ax[0].set_title('Students vs test preparation course ',color='black',size=25)
+for container in ax[0].containers:
+    ax[0].bar_label(container,color='black',size=20)
+    
+sns.countplot(x=df['parental level of education'],data=df,palette = 'bright',hue='lunch',saturation=0.95,ax=ax[1])
+for container in ax[1].containers:
+    ax[1].bar_label(container,color='black',size=20)
+
+##TEST PREPARATION COURSE COLUMN
+#BIVARIATE ANALYSIS ( Is Test prepration course has any impact on student's performance ? )
+plt.figure(figsize=(12,6))
+plt.subplot(2,2,1)
+sns.barplot (x=df['lunch'], y=df['math score'], hue=df['test preparation course'])
+plt.subplot(2,2,2)
+sns.barplot (x=df['lunch'], y=df['reading score'], hue=df['test preparation course'])
+plt.subplot(2,2,3)
+sns.barplot (x=df['lunch'], y=df['writing score'], hue=df['test preparation course'])
+
+###CHECKING OUTLIERS
+plt.subplots(1,4,figsize=(16,5))
+plt.subplot(141)
+sns.boxplot(df['math score'],color='skyblue')
+plt.subplot(142)
+sns.boxplot(df['reading score'],color='hotpink')
+plt.subplot(143)
+sns.boxplot(df['writing score'],color='yellow')
+plt.subplot(144)
+sns.boxplot(df['average'],color='lightgreen')
+plt.show()
+
+###MUTIVARIATE ANALYSIS USING PAIRPLOT
+sns.pairplot(df,hue = 'gender')
+plt.show()
+
+"""Conclusions
+Student's Performance is related with lunch, race, parental level education
+Females lead in pass percentage and also are top-scorers
+Student's Performance is not much related with test preparation course
+Finishing preparation course is benefitial.
+"""
